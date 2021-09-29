91217fa0e44b9b22350555468f6ffcd260f38232
Daniel <danielbebon@gmail.com>
2021-09-29 13:16:51 -0500

    2.0
    Añadí la condición para que el usuario elija cuantas notas va a promediar, y el bucle do while para hacer mas de un calculo.

--- Algoritmo calificacion en formato de letras.c
+++ Algoritmo calificacion en formato de letras.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 int main ()
-{
-		//Calcular calificaciones en A, B, C, D y F
-		
-	float nota1, nota2, nota3, nota4, nota5, promedio;
-	printf("Introduzca la primera nota\n-> ");
-	scanf("%f",&nota1);
-	printf("Introduzca la segunda nota\n-> ");
-	scanf("%f",&nota2);
-	printf("Introduzca la tercera nota\n-> ");
-	scanf("%f",&nota3);
-	printf("Introduzca la cuarta nota\n-> ");
-	scanf("%f",&nota4);
-	printf("Introduzca la quinta nota\n-> ");
-	scanf("%f",&nota5);
-	promedio = (nota1+nota2+nota3+nota4+nota5)/5;
-	
-	if(promedio>=90){
-		printf("Su calificacion es\n->A");
-	}
-	else if(promedio>=80){
-		printf("Su calificacion es\n->B");
-	}
-	else if(promedio>=70){
-		printf("Su calificacion es\n->C");
-	}
-	else if(promedio>=60){
-		printf("Su calificacion es\n->D");
-	}
-	else if(promedio<=59){
-		printf("Su calificacion es\n->F");
-	}
+{	
+	float suma, promedio;
+	int i,cant,cal1, rep;
+	do{
+		printf("Bienvenido!!!\n");
+		printf("\nDigite la cantidad de notas que va a ingresar\n: ");
+		scanf("%d",&cant);
+		suma=0.0;
+		for(i=0;i<cant;i++){
+			printf("\nDigite su calificacion %i\n: ", i+1);
+			scanf("%d",&cal1);
+			suma=suma+cal1;
+		}
+		promedio=suma/cant;
+		if(promedio>=90){
+			printf("Su calificacion es\n->A");
+		}
+		else if(promedio>=80){
+			printf("Su calificacion es\n->B");
+		}
+		else if(promedio>=70){
+			printf("Su calificacion es\n->C");
+		}
+		else if(promedio>=60){
+			printf("Su calificacion es\n->D");
+		}
+		else if(promedio<=59){
+			printf("Su calificacion es\n->F");
+		}
+		printf("\nDigite 1 para CONTINUAR --- 2 para SALIR: ");scanf("%d",&rep);
+		printf("\n----------------------------------\n");
+	}while(rep==1);
 	return 0;
-	
 }
